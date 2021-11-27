d4342d17fcdee6dbdd01756d790e0bfb75f6ccc3
Anamariavenegas <avenegasp0205@hotmail.com>
2021-11-27 14:21:21 -0500

    RETOHU100

---
+++ Hackaton/Hackaton.App.Dominio/Entidades/uSistemas.cs
@@ -0,0 +1,12 @@
+using System;
+namespace Hackaton.App.Dominio{
+    public class uSistemas{
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string apellidos { get; set; }
+        public string tipo_documento { get; set; }
+        public string identificacion { get; set; }
+        public string email { get; set; }
+    }
+ 
+}
